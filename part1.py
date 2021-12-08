effcc83fd1f3b828a880468ca341ff17cd1503dc
Jacobnegroni <89854940+Jacobnegroni@users.noreply.github.com>
2021-12-08 13:11:22 -0500

    Update part1.py

--- part1.py
+++ part1.py
@@ -9,12 +9,15 @@ def setUpDatabase(db_name):
     conn = sqlite3.connect(path+'/'+db_name)
     cur = conn.cursor()
     return cur, conn
+# The function looks at the current path and sets up the database. A connection is made to the database using the connect() method and a cursor object is defined. 
+
 
 def getdataAPI(url):
     page = requests.get(url)
     soup = BeautifulSoup(page.content,'html.parser')
     data = json.loads(soup.text)
     return data['data']
+# The function takes a url string and uses the get() method from requests module to retrieve the webpage. Content parsing using BeautifulSoup. The json module loads() method is used to convert the text into a list of dictionaries.
 
 def setUpPopulationTable(data_json, cur, conn, limit=20):
     cur.execute("CREATE TABLE IF NOT EXISTS Population (id TEXT PRIMARY KEY, year INTEGER, state TEXT, population INTEGER)")
@@ -35,6 +38,7 @@ def setUpPopulationTable(data_json, cur, conn, limit=20):
     nrows = len(cur.fetchall())
     conn.commit()
     print(str(nrows)+"/364 entries in the Population table")
+#  Creates the Table Population in the project.db database if it does not exist. Each dictionary in the list contains the keys (ID State, Year, State,P opulation). If the new total number of rows equals the nrows+limit number, the for loop is exited by using the break command. Changes are committed.
 
 def setUpPovertyTable(data_json, cur, conn, limit=20):
     cur.execute("CREATE TABLE IF NOT EXISTS Poverty (id TEXT PRIMARY KEY, year INTEGER, state TEXT, poverty_rate INTEGER)")
@@ -56,7 +60,7 @@ def setUpPovertyTable(data_json, cur, conn, limit=20):
     conn.commit()
     print(str(nrows)+"/364 entries in the Poverty table")
 
-
+# Creates the table for poverty data.  Very similar to setUpPopulationTable() function but instead of the population field, the ‘poverty rate’ is considered. Existing entries are ignored, new entries are added and the for loop is exited using the break command. Changes are committed.
 # Database
 cur, conn = setUpDatabase('project.db')
 
